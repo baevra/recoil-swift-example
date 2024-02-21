3dcc2d1b13efa85c4ce1b98a66bafa2553d8c1ea
Roman Baev <baevra@gmail.com>
2024-02-21 16:18:31 +0700

    Add xcframework

---
+++ RecoilSwiftFramework.xcframework/ios-arm64_x86_64-simulator/RecoilSwiftFramework.framework/Headers/RecoilSwiftFramework.h
@@ -0,0 +1,18 @@
+//
+//  RecoilSwiftFramework.h
+//  RecoilSwiftFramework
+//
+//  Created by Roman Baev on 21.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecoilSwiftFramework.
+FOUNDATION_EXPORT double RecoilSwiftFrameworkVersionNumber;
+
+//! Project version string for RecoilSwiftFramework.
+FOUNDATION_EXPORT const unsigned char RecoilSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecoilSwiftFramework/PublicHeader.h>
+
+
